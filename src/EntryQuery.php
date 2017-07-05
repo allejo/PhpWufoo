3ef583cd47c741d031ca75f70c2fb03b17f58c1c
allejo <allejo@me.com>
2017-07-05 09:32:43 -0700

    Add phpDocs for EntryQuery

--- src/EntryQuery.php
+++ src/EntryQuery.php
@@ -2,6 +2,12 @@
 
 namespace allejo\Wufoo;
 
+/**
+ * An EntryQuery is used to specify how the results will be fetched and what entry filters will be used.
+ *
+ * @api
+ * @since 0.1.0
+ */
 class EntryQuery
 {
     private $filters;
@@ -11,6 +17,11 @@ class EntryQuery
     private $pageStart;
     private $pageSize;
 
+    /**
+     * @api
+     *
+     * @since 0.1.0
+     */
     public function __construct()
     {
         $this->filters = [];
@@ -50,6 +61,18 @@ class EntryQuery
         return urldecode(http_build_query($query));
     }
 
+    /**
+     * Filter the results based on entry filters.
+     *
+     * @api
+     *
+     * @param EntryFilter|EntryFilter[] $filters
+     * @param bool                      $booleanAnd
+     *
+     * @since 0.1.0
+     *
+     * @return $this
+     */
     public function where($filters, $booleanAnd = true)
     {
         $this->booleanAnd = (bool)$booleanAnd;
@@ -68,6 +91,21 @@ class EntryQuery
         return $this;
     }
 
+    /**
+     * When paginating results of a query, set the offset.
+     *
+     * **Warning:** This function should _not_ be used in conjunction with EntryQuery::paginate().
+     *
+     * @api
+     *
+     * @param int $offset
+     *
+     * @throws \InvalidArgumentException if $offset is not an integer
+     *
+     * @since 0.1.0
+     *
+     * @return $this
+     */
     public function offset($offset)
     {
         if (!is_int($offset))
@@ -80,6 +118,25 @@ class EntryQuery
         return $this;
     }
 
+    /**
+     * Set the number of results to be returned in a query.
+     *
+     * **Warnings**
+     *
+     * - The API restricts this value to a maximum of 100. However, this function will not impose any restrictions should
+     *   the API change this restriction
+     * - This function should _not_ bt used in conjunction with EntryQuery::paginate()
+     *
+     * @api
+     *
+     * @param int $limit
+     *
+     * @throws \InvalidArgumentException if $limit is not an integer
+     *
+     * @since 0.1.0
+     *
+     * @return $this
+     */
     public function limit($limit)
     {
         if (!is_int($limit))
@@ -92,6 +149,23 @@ class EntryQuery
         return $this;
     }
 
+    /**
+     * Define the pagination for this query.
+     *
+     * **Warning:** This function is provided as a convenience function that will set both the offset and limit in one
+     * function call. This should be instead of calling EntryQuery::offset() and EntryQuery::limit() separately.
+     *
+     * @api
+     *
+     * @param int $offset
+     * @param int $limit
+     *
+     * @throws \InvalidArgumentException if $offset or $limit are not integers
+     *
+     * @since 0.1.0
+     *
+     * @return $this
+     */
     public function paginate($offset, $limit)
     {
         $this->limit($limit);
@@ -100,6 +174,18 @@ class EntryQuery
         return $this;
     }
 
+    /**
+     * Sort the results based on a field.
+     *
+     * @api
+     *
+     * @param string $field     The API Field ID to sort by
+     * @param bool   $ascending Set to true to sort in ascending order
+     *
+     * @since 0.1.0
+     *
+     * @return $this
+     */
     public function sortBy($field, $ascending = true)
     {
         $this->ascending = $ascending;
@@ -116,6 +202,11 @@ class EntryQuery
         }
     }
 
+    /**
+     * Convenience function to create an EntryQuery that be used for immediate chaining.
+     *
+     * @return EntryQuery
+     */
     public static function create()
     {
         $query = new self();
